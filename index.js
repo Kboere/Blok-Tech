fde0b9925b9ea2f6594f6e32f01a81f222844136
Kboere <kevin.boere@hva.nl>
2023-02-22 17:58:19 +0100

    EJS template engine added + Tests pages

--- index.js
+++ index.js
@@ -1,18 +1,55 @@
 const express = require('express')
+const bodyParser = require('body-parser')
+const path = require("path")
 const app = express()
 const port = 3000
 
-app.use(express.static('static'))
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://KevinBoere:K3v1n@701@cluster0.itavqik.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
+// });
 
-app.get('/hallo', (req, res) => {
-  res.send('<img src="/img/duck.jpeg" width="150px">Hello World!')
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.use(express.static(__dirname + '/static'))
+
+// use res.render to load up an ejs view file
+
+// index page
+app.get('/', function(req, res) {
+  res.render('pages/index')
 })
+
+// about page
+app.get('/zoekresultaat', function(req, res) {
+  res.render('pages/zoekresultaat')
+})
+
+app.post("/login", (req, res) => {
+  const { name, password } = req.body;
+
+  if (name === "admin" && password === "admin") {
+    res.render("pages/succes", {
+      username: name,
+    });
+  } else {
+    res.render("pages/failure");
+  }
+});
+
+
+app.use(function (req, res, next) {
+  // YOU CAN CREATE A CUSTOM EJS FILE TO SHOW CUSTOM ERROR MESSAGE
+    res.status(404).render('pages/404page')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
-})
-
-app.get('/home/:user', (req, res) => {
-    console.log(`input van ${req.params.user}`)
-    res.send(`<img src="/img/duck.jpeg" width="150px"><h1>Hello `+ req.params.user + `</h1>`)
 })
