79fe615030cb39af58c4e5877ff2eba492d0d258
Kboere <kevin.boere@hva.nl>
2023-03-02 11:22:15 +0100

    env file

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
+require('dotenv').config()
 const express = require('express')
 // const bodyParser = require('body-parser') is niet meer nodig
 // const path = require('path')
 const app = express()
 const port = 3000
 
+const { MongoClient, ServerApiVersion } = require('mongodb')
 
+const uri = 'mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@logindatabase.oo6xrnt.mongodb.net/?retryWrites=true&w=majority'
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
+client.connect(err => {
+  const collection = client.db("logindatabase").collection("user")
+  // perform actions on the collection object
+  // client.close();
+})
 
 app.use(express.urlencoded({extended: true}))
 
