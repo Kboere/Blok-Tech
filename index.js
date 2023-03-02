3669d805c0f3a3b0c5a07977e12c3fda95295634
Kboere <kevin.boere@hva.nl>
2023-03-02 11:06:45 +0100

    ejs files added

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express = require('express')
-const bodyParser = require('body-parser')
-const path = require("path")
+// const bodyParser = require('body-parser') is niet meer nodig
+// const path = require('path')
 const app = express()
 const port = 3000
 
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://KevinBoere:K3v1n@701@cluster0.itavqik.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-// client.connect(err => {
-//   const collection = client.db("test").collection("devices");
-//   // perform actions on the collection object
-//   client.close();
-// });
 
-app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use(express.urlencoded({extended: true}))
 
 // set the view engine to ejs
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs')
 
 app.use(express.static(__dirname + '/static'))
 
@@ -27,23 +20,25 @@ app.get('/', function(req, res) {
   res.render('pages/index')
 })
 
-// about page
+// zoekresultaten page
 app.get('/zoekresultaat', function(req, res) {
   res.render('pages/zoekresultaat')
 })
 
-app.post("/login", (req, res) => {
-  const { name, password } = req.body;
+app.post("/zoekresultaat", (req, res) => {
+  console.log(req.body)
+  res.render("pages/zoekresultaat")
 
-  if (name === "admin" && password === "admin") {
-    res.render("pages/succes", {
-      username: name,
-    });
-  } else {
-    res.render("pages/failure");
-  }
+  // const { name, password } = req.body;
+
+  // if (name === "admin" && password === "admin") {
+  //   res.render("pages/succes", {
+  //     username: name,
+  //   });
+  // } else {
+  //   res.render("pages/failure");
+  // }
 });
-
 
 app.use(function (req, res, next) {
   // YOU CAN CREATE A CUSTOM EJS FILE TO SHOW CUSTOM ERROR MESSAGE
