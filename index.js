ebf06ccefc8bce724345b731cd9422638b2d1512
Kboere <kevin.boere@hva.nl>
2023-03-07 17:56:26 +0100

    fetch data from database fail

--- index.js
+++ index.js
@@ -1,95 +1,81 @@
 require('dotenv').config()
-const { MongoClient } = require('mongodb')
+
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const path = require('path')
 const port = 3000
 
-// Replace the uri string with your MongoDB deployment's connection string.
+// Connecting mongoDB
+const { MongoClient, ServerApiVersion } = require('mongodb')
 const uri = process.env.MONGODB_URI
-const client = new MongoClient(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 })
 const dbName = 'test'
-// const db = client.db(dbName)
 
-// ik zou hier bij zetten wat dit doet
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static(__dirname + '/static'))
+// collections aanroepen
+const db = client.db(dbName)
+const colf = db.collection('form')
+const cole = db.collection('events')
 
 // set the view engine to ejs
 app.set('view engine', 'ejs')
 
-// use res.render to load up an ejs view file
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname, '/static')))
+
 // index page
 app.get('/', function (req, res) {
   res.render('pages/index')
 })
 
 // zoekresultaten page
-app.get('/zoekresultaat', function (req, res) {
-  res.render('pages/zoekresultaat')
+app.get('/zoekresultaat', async (req, res) => {
+  try {
+    await client.connect()
+    console.log('Connected correctly to server')
 
-  async function run() {
-    try {
-      await client.connect()
-      console.log('Connected correctly to server')
-      const db = client.db(dbName)
-      // Use the collection'events'
-      const col = db.collection('events')
-
-      const cursor = col.find()
-
-      // print a message if no documents were found
-      if ((await col.countDocuments()) === 0) {
-        console.log('No documents found!')
-      }
-      // replace console.dir with your callback to access individual elements
-      await cursor.forEach(console.dir)
-    } finally {
-      await client.close()
-    }
+    const data = await cole.find({ reistijd: '53km' })
+    console.log(data)
+    res.render('pages/zoekresultaat', { data: data })
+  } catch (err) {
+    console.log(err.stack)
   }
-  run().catch(console.dir)
 })
 
-// de console.log en async function zitten niet op dezelfde tab en dat is niet netjes
-// ook wordt de functie best wel lang en onleesbaar zo
-// want runt de functie
-app.post('/zoekresultaat', (req, res) => {
-  console.log(req.body)
+app.post('/zoekresultaat', async (req, res) => {
+  try {
+    await client.connect()
+    // console.log('Connected correctly to server')
 
-  async function run() {
-    try {
-      await client.connect()
-      console.log('Connected correctly to server')
-      const db = client.db(dbName)
-      // Use the collection'form'
-      const col = db.collection('form')
-
-      // Construct a document
-      const personDocument = {
-        'Antwoord op vraag 1': req.body.vraag1,
-        'Antwoord op vraag 2': req.body.vraag2,
-        'Antwoord op vraag 3': req.body.vraag3,
-        'Antwoord op vraag 4': req.body.vraag4,
-        'extra aanvulling van vraag 4': req.body.recommend
-      }
-      // Insert a single document, wait for promise so we can read it back
-      const p = await col.insertOne(personDocument)
-    } catch (err) {
-      console.log(err.stack)
-    } finally {
-      await client.close()
+    // Construct a document
+    const personDocument = {
+      AntwoordVraag1: req.body.vraag1,
+      AntwoordVraag2: req.body.vraag2,
+      AntwoordVraag3: req.body.vraag3,
+      AntwoordVraag4: req.body.vraag4
     }
+    // Insert a single document, wait for promise so we can read it back
+    await colf.insertOne(personDocument)
+
+    const data = await cole.find({ reistijd: '53km' })
+    res.render('pages/zoekresultaat', { data: data })
+
+    console.log(personDocument)
+    console.log(data)
+  } catch (err) {
+    console.log(err.stack)
+  } finally {
+    await client.close()
   }
-  run().catch(console.dir)
-  res.render('pages/zoekresultaat')
+  // res.render('pages/zoekresultaat')
 })
+
 app.use(function (req, res, next) {
-  // YOU CAN CREATE A CUSTOM EJS FILE TO SHOW CUSTOM ERROR MESSAGE
   res.status(404).render('pages/404page')
 })
 
+// Altijd onderaan zetten
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
