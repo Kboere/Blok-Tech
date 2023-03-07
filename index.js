5966f1a531b0052b800f6066e5864baf2a6a0fb0
Kboere <kevin.boere@hva.nl>
2023-03-07 12:45:19 +0100

    data from database added

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = 3000
 const uri = process.env.MONGODB_URI
 const client = new MongoClient(uri)
 const dbName = 'test' 
+const db = client.db(dbName)
 
+// ik zou hier bij zetten wat dit doet
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static(__dirname + '/static'))
@@ -23,14 +25,51 @@ app.get('/', function(req, res) {
   res.render('pages/index')
 })
 
+
+const posts = [
+  {title: 'Title 1', body: 'Body 1' },
+  {title: 'Title 2', body: 'Body 2' },
+  {title: 'Title 3', body: 'Body 3' },
+  {title: 'Title 4', body: 'Body 4' },
+]
+const user = {
+  firstName: 'Tim',
+  lastName: 'Cook',
+}
+
 // zoekresultaten page
 app.get('/zoekresultaat', function(req, res) {
-  res.render('pages/zoekresultaat')
+  res.render('pages/zoekresultaat',{ articles: posts})
+
+//   async function run() {
+//     try {
+//          await client.connect() 
+//          console.log('Connected correctly to server') 
+//          const db = client.db(dbName) 
+//          // Use the collection'form'
+//          const col = db.collection('events') 
+
+//          const cursor = col.find()
+//          // print a message if no documents were found
+//          if ((await col.countDocuments()) === 0) {
+//            console.log("No documents found!")
+//          }
+//          // replace console.dir with your callback to access individual elements
+//          await cursor.forEach(console.dir)
+//        } finally {
+//          await client.close();
+//        }
+// }
+// run().catch(console.dir)
+
 })
 
+// de console.log en async function zitten niet op dezelfde tab en dat is niet netjes
+// ook wordt de functie best wel lang en onleesbaar zo
+// want runt de functie 
 app.post('/zoekresultaat', (req, res) => {
   console.log(req.body)
-                      
+    
  async function run() {
     try {
          await client.connect() 
@@ -59,9 +98,7 @@ app.post('/zoekresultaat', (req, res) => {
     }
 }
 run().catch(console.dir) 
-
 res.render('pages/zoekresultaat')
-
 }) 
 
 app.use(function (req, res, next) {
